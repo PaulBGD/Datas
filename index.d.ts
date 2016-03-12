9c3dce13fd3df0e69e5f4cd05074dbd586ec56d5
Paul Sauve <paul@burngames.net>
2016-03-12 14:48:34 -0600

    Add type for merge

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,7 @@
 export default class DatasStore<T> {
     constructor(initialState: T);
     setState(state: T): void;
+    merge(partialState: T): void;
     getState(): T;
     hasChanged(nextState: T): boolean;
     watch(listener: (state: T) => any): () => void;
